9a1a033d8d2acb04fb4b45a6aaba59721468b48a
ReyIntelCorei7 <reynarapratama@gmail.com>
2026-01-11 17:34:50 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Page Title' }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-100">
+    <div class="container mx-auto p-4">
+
+        <h1 class="text-3xl font-bold mb-4">{{ $title ?? 'Welcome to My Application' }}</h1>
+
+        <div class="bg-white p
+    </div>
+</body>
+
+</html>
