98f8d7dbf483aabe147ec6c4203bd45cc5d17d94
gregoriusreynarapratama-del <gregoriusreynarapratama@gmail.com>
2026-01-30 18:06:27 +0700

    tak tau

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 import './bootstrap';
-import Alpine from 'alpinejs'
-import intersect from '@alpinejs/intersect' // Pastikan baris ini ada
- 
-Alpine.plugin(intersect) // Pastikan plugin didaftarkan sebelum Alpine.start()
- 
-window.Alpine = Alpine
-Alpine.start()
+import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
+
+Alpine.plugin(intersect);
+window.Alpine = Alpine;
+Alpine.start();
